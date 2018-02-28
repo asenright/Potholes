671d35f61a1352a6a26126fa3fd48be2e6e3cfd7
asenright <asenright@gmail.com>
2018-02-28 15:39:39 -0800

    Added Datasheets and gps.py
    Added datasheets for the raspberry pi 3 and the GP-20U7. Added Python script gps.py that saves gps data.

---
+++ gps.py
@@ -0,0 +1,55 @@
+#gps.py
+#A script to pull GPS data.
+# Uses the pynmea2 library.
+# Pynmea2 can be installed via "pip install pynmea2".
+#
+#
+
+#! /usr/bin/env python
+import serial
+import csv
+import pynmea2
+import sys
+
+ser = serial.Serial(
+	port='/dev/ttyS0',
+	baudrate=9600,
+	parity=serial.PARITY_NONE,
+	stopbits=serial.STOPBITS_ONE,
+	bytesize=serial.EIGHTBITS,
+	timeout=1
+)
+
+print "Is Serial Open? " + str(ser.isOpen())
+
+#print "Read ten lines:"
+#for x in range(0, 10):
+#	print "%d : " %(x) + ser.readline()
+#ser.close()
+
+
+
+from datetime import datetime
+curDT = datetime.utcnow().strftime("%Y-%m-%d %X")
+print "Current DT: "+curDT
+with open("test.csv", 'wb') as csvfile:
+	csvWriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+	headers = ["datetime", "num_sats", "latitude", "latitude_dir", "longitude", "longitude_dir"]
+	csvWriter.writerow(headers)
+	while True:
+		try: 
+			line = ser.readline()
+			print line	
+			if '$GPGGA' in line:
+				#print line
+				msg = pynmea2.parse(line)
+				print "---Number of satellites in comms range: " + msg.num_sats
+				if msg.num_sats == "00":
+					print "!!! Warning !!! No satellites in range, long and lat likely inaccurate"
+				print "---Latitude: " + msg.lat + " " + msg.lat_dir
+				print "---Longitude: " + msg.lon + " " + msg.lon_dir
+				curRow = [curDT, msg.num_sats, msg.lat, msg.lat_dir, msg.lon, msg.lon_dir]
+				csvWriter.writerow(curRow)
+		except (RuntimeError, TypeError, NameError):
+			print "General exception. Closing down."
+			raise SystemExit
